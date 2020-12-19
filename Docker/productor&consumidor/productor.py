4260b7aec99e272ebea6d05d82b28e395e59b991
albertorz31 <alberto.rodriguez.z@usach.cl>
2020-12-19 17:17:18 -0300

    productor

--- Docker/productor&consumidor/productor.py
+++ Docker/productor&consumidor/productor.py
@@ -1,9 +1,32 @@
 import praw
 from kafka import KafkaProducer
 
+#Se crea productor
 producer = KafkaProducer(bootstrap_servers = 'localhost:9092', value_serializer = lambda x: dumps(x).encode('utf-8'))
 
 reddit = praw.Reddit(client_id='V5uor3c5ZmD4nQ',client_secret='ljuy3utnTTcQAV-dzH0642MaXAuciQ',user_agent='lab_distribuidos')
 
-comments = reddit.redditor("nottheonion").comments.new(limit=None)
+#Esto te muestra los comentarios de un usuario en especifico, pero no lo uso para filtrar
+#comments = reddit.redditor("spez").comments.new(limit=None)
+
+
+#Se filtra los comentarios de la subreddit 
+for comment in reddit.subreddit("learnpython").stream.comments():
+    submission = reddit.submission(id=comment.submission) 
+    if submission.over_18 == True:
+        print("filtrado\n")
+    else:
+        print("comentario\n")
+        commentAuthor = comment.author
+        commentBody = comment.body
+        commentScore = comment.score
+        commentId = comment.id
+        submissionId = submission.id
+        submissionTitle = submission.title
+        subreddit = comment.subreddit
+        data = {'comment': commentBody, 'author': str(commentAuthor), 'score': commentScore, 'id': commentId, 'idSubmission': submissionId, 'submission': str(submissionTitle), 'subreddit': str(subreddit)}
+        print(data,"\n")
+        #Se publican los comentarios en el topico "comentarios"
+        producer.send('comentarios', value = data)
+
 
