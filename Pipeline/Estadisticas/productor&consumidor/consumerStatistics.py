319c3fb77a95b13a2300836f960f68166a7a3da0
albertorz31 <alberto.rodriguez.z@usach.cl>
2020-12-20 02:42:21 -0300

    carpetas pipeline

---
+++ Pipeline/Estadisticas/productor&consumidor/consumerStatistics.py
@@ -0,0 +1,10 @@
+import praw
+from kafka import KafkaConsumer
+from json import loads
+
+consumer = KafkaConsumer('estadisticas',bootstrap_servers=['localhost:9091'],auto_offset_reset='earliest',enable_auto_commit=True,
+						 value_deserializer=lambda x: loads(x.decode('utf-8')),api_version=(0, 10, 1))
+
+
+for message in consumer:
+    print(message)
---
+++ Pipeline/Estadisticas/productor&consumidor/productorStatistics.py
@@ -0,0 +1,5 @@
+from kafka import KafkaProducer
+from json import dumps
+
+#Se crea productor
+producer = KafkaProducer(bootstrap_servers = 'localhost:9092', value_serializer = lambda x: dumps(x).encode('utf-8'),api_version=(0, 10, 1))
