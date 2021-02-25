27903e92cdc17040ede0f13b7fdb352ce4638a34
Valentina Ligueño <valentina.ligueno@usach.cl>
2021-02-25 17:55:00 -0300

    Se produce y consume y se guarda en mongo

--- Pipeline/Comentarios/productor&consumidor/consumerComment.py
+++ Pipeline/Comentarios/productor&consumidor/consumerComment.py
@@ -6,21 +6,35 @@ import pymongo
 from pymongo import MongoClient
 from pymongo import *
 
-consumer = KafkaConsumer('comentarios',bootstrap_servers=['localhost:9092'],auto_offset_reset='earliest',enable_auto_commit=True,
-						 value_deserializer=lambda x: loads(x.decode('utf-8')),api_version=(0, 10, 1))
+print("test\n")
+consumer = KafkaConsumer(
+                        'comentarios',
+                        bootstrap_servers=['kafka:9092'],
+                        auto_offset_reset='latest',
+                        enable_auto_commit=True,
+						value_deserializer=lambda x: loads(x.decode('utf-8')),
+                        api_version=(0, 10, 1))
 
 
-client = MongoClient('localhost', username='root', password='distribuidos', authSource='db', authMechanism='SCRAM-SHA-1') #datos de conexion de bd
+client = MongoClient(
+                    '172.20.0.1', 
+                    username="root", 
+                    password="distribuidos", 
+                    authSource='admin') #datos de conexion de bd
 db = client["db"] #asi se accede a una bd
 collection = db["reddit_comments"] #asi se accede a una coleccion
 
 for message in consumer:
-    insertMongo = {"id": message.value['id'],
+    #event_data = event.value
+    print("Soy el consumidor y recibí este comentario:\n")
+    
+    print(message.value['id'])
+    insertMongo = {
+                    "id": message.value['id'],
                     "author": message.value['author'], 
 					"body": message.value['comment'],
 					"score": message.value['score'],
                     "subreddit": message.value['subreddit'],
 					"post": message.value['submission']
                     }           
-    print(message)
     collection.insert_one(insertMongo)
--- Pipeline/Comentarios/productor&consumidor/productorComment.py
+++ Pipeline/Comentarios/productor&consumidor/productorComment.py
@@ -3,9 +3,16 @@ from kafka import KafkaProducer
 from json import dumps
 
 #Se crea productor
-producer = KafkaProducer(bootstrap_servers = 'localhost:9092', value_serializer = lambda x: dumps(x).encode('utf-8'),api_version=(0, 10, 1))
+producer = KafkaProducer(
+            bootstrap_servers = 'kafka:9092', 
+            value_serializer = lambda x: dumps(x).encode('utf-8'),
+            api_version=(0, 10, 1)
+            )
 
-reddit = praw.Reddit(client_id='V5uor3c5ZmD4nQ',client_secret='ljuy3utnTTcQAV-dzH0642MaXAuciQ',user_agent='lab_distribuidos')
+reddit = praw.Reddit(
+            client_id='V5uor3c5ZmD4nQ',
+            client_secret='ljuy3utnTTcQAV-dzH0642MaXAuciQ',
+            user_agent='lab_distribuidos')
 
 #Esto te muestra los comentarios de un usuario en especifico, pero no lo uso para filtrar
 #comments = reddit.redditor("spez").comments.new(limit=None)
@@ -25,8 +32,10 @@ for comment in reddit.subreddit('all').stream.comments():
         submissionId = submission.id
         submissionTitle = submission.title
         subreddit = comment.subreddit
+        #Comentario a enviar
         data = {'comment': commentBody, 'author': str(commentAuthor), 'score': commentScore, 'id': commentId, 'idSubmission': submissionId, 'submission': str(submissionTitle), 'subreddit': str(subreddit)}
         print(data,"\n")
+
         #Se publican los comentarios en el topico "comentarios"
         producer.send('comentarios', value = data)
 
