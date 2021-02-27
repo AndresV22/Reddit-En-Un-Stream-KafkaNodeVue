51a2ef0daf36e96e4538a8a6658e682a13416baa
Valentina Ligueño <valentina.ligueno@usach.cl>
2021-02-27 16:19:22 -0300

    Obteniendo documentos de mongodb

--- Pipeline/Estadisticas/productor&consumidor/productorStatistics.py
+++ Pipeline/Estadisticas/productor&consumidor/productorStatistics.py
@@ -1,7 +1,44 @@
 from kafka import KafkaProducer
 from json import dumps
 
+import pymongo
+from pymongo import MongoClient
+from pymongo import *
+
 #Se crea productor
-producer = KafkaProducer(bootstrap_servers = 'localhost:9092', value_serializer = lambda x: dumps(x).encode('utf-8'),api_version=(0, 10, 1))
+'''producer = KafkaProducer(
+              bootstrap_servers = 'kafka:9092', 
+              value_serializer = lambda x: dumps(x).encode('utf-8'),
+              api_version=(0, 10, 1))'''
 
+client = MongoClient(
+                    '172.20.0.1', 
+                    username="root", 
+                    password="distribuidos", 
+                    authSource='admin') #datos de conexion de bd
+db = client["db"] #asi se accede a una bd
+collection = db["reddit_comments"] #asi se accede a una coleccion
+reddit_comments = collection.find()
 
+for comment in reddit_comments:
+  print("Soy el productor y estoy enviando el siguiente mensaje:\n")
+  print(comment)
+  #Para obtener atributo del documento obtenido de la base de datos
+  id = comment['id']
+  author = comment['author'] 
+  body = comment['body']
+  score = comment['score']
+  subreddit = comment['subreddit']
+  post = comment['post']
+
+  data = {'id': id,
+          'comment': body, 
+          'author': author, 
+          'score': score, 
+          'submission': post, 
+          'subreddit': subreddit
+        }
+  
+  
+  #Se envían los comentarios al topico "estadisticas"
+  #producer.send('stats', value = data)
